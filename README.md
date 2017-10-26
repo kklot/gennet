c914aedffd185fc339b4ab2637cd85f3412345d1
Kinh <nguyenkinh@ytecongcong.com>
2017-10-26 13:08:06 +0200

    Add codes

--- README.md
+++ README.md
@@ -1,7 +1,99 @@
 # gennet
 Generating network age-specific
 
-# Supplemental codes
+# Licencing
 
 <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License</a>.
 
+## Requirements
+
+- a target population age-structure
+- contact distribution (Mossong et al Plos Medicine)
+- contact matrix (Mossong et al Plos Medicine)
+
+```
+N    <- 1000
+seed <- 123
+```
+
+## Steps
+
+Assign age: depends on the target population age distribution
+
+```
+  SR             <- dget("SR")  # Sierra Leon population
+  if (!is.null(seed)) set.seed(seed)
+  age           <- sample(SR$age, N, TRUE, SR$prob)
+  # add ages 70/80+
+  age[age==70] <- sample(70:80, length(age[age==70]), replace=1)
+```
+
+Assign #contacts: depends on both target age and POLYMOD data
+```
+  agecont      <- orderBy(dget("POLYMODtab1"), 1) # POLYMOD Tab.1
+  agecont$rk   <- rank(agecont$age)  # Rank age-group by contact
+  POLYMODbreak <- c(0,4,9,14,19,29,39,49,59,69,80) 
+  ageGrp <- cut(age, breaks=POLYMODbreak, include.lowest=1)
+  ageGrp <- as.numeric(ageGrp) # mapping age to ageGrp
+  agerk  <- sapply(ageGrp, function(x) agecont$rk[x==agecont$nmr])
+  age    <- rev(orderBy(cbind(age, agerk), 2)[, 1]) # large to small
+```
+
+Sampling from contact distribution
+
+```
+  distCont <- dget("distCont")
+  ncont    <- sort(sample(distCont$freq, N, 1, distCont$prob), TRUE)
+```
+
+Load contact matrix (as probability) POLYMOD data: averaging all countries
+
+```
+  M <- dget("M")  # image(M)
+  Mbrk <- c(0,4,9,14,19,24,29,34,39,44,49,54,59,64,69,70)
+  Mnmr <- 1:15
+  Grp  <- as.numeric(cut(age, breaks=Mbrk, include.lowest=1))
+  Grp[is.na(Grp)] <- 15 # 70+ age-grp
+```
+
+Run
+
+```
+source("gennet.R")
+g <- genNet(N, age)
+```
+
+## Outputs
+
+```
+plotNet(g)
+```
+
+![](g1.png)
+
+## Validate
+
+Compare age-distribution
+```
+ks.test(age, vertex_attr(g, "age"))
+
+Two-sample Kolmogorov-Smirnov test
+
+data:  age and vertex_attr(g, "age")
+D = 0, p-value = 1
+alternative hypothesis: two-sided
+
+```
+
+Compare contact-distribution
+```
+ks.test(degree(g), ncont)
+
+Two-sample Kolmogorov-Smirnov test
+
+data:  degree(g) and ncont
+D = 0, p-value = 1
+```
+
+Compare contact-matrix
+![](matrixCompare.png)
---
+++ gennet.R
@@ -0,0 +1,52 @@
+genNet <- function(N=N, age=age, n.try=100) {
+  require(igraph) # TODO: add check points for invalid inputs
+  if (N < 100) warning("Considering increase N!")
+  # Utils functions -------------------------------------------------------
+  `%notin%` <- function(x, y) is.na(match(x, y, nomatch=NA_integer_))
+  # pick age to make contacts according the prob. from contact matrix
+  pickAge <- function(x) sample(Mnmr, ncont[x], 1, M[, Grp[x]])
+  # pick id that is free and in respected age groups
+  pickId <- function(x) {
+    pool <- freeI[freeI %in% which(Grp==apick[x])]
+    if (length(pool)==1) return(pool)
+      else return(sample(pool, npick[x]))
+  }
+  makeLinks <- function(i, j) c(sapply(j, function(x) c(i, x)))
+  # templates and storages ------------------------------------------------
+  g <- make_empty_graph(n = N, directed = FALSE)
+  # g <- set_vertex_attr(g, 'n', value=ncont)
+  freeI <- which(ncont > 0) # some might have no contact
+  # Main loop -------------------------------------------------------------
+  for (i in 1:N) {
+    cat('\r', "Node:", i); flush.console()
+    if (ncont[i] == 0) next # am I free?
+    freeI <- freeI[freeI %notin% i] # exclude myself: no loop back
+    # Check if enough peoples to contact ----------------------------------
+    freeA <- rle(sort(Grp[freeI])) #freenodes by age-grp
+    afree <- freeA$values  # age-grp that have freenodes
+    nfree <- freeA$lengths  # of freenodes by ages
+    isEnough <- noWay <- FALSE
+    i.try    <- 0  # trying to find enough matched age partners
+    while(!isEnough) {
+      i.try <- i.try + 1
+      if ( i.try > n.try ) isEnough <- noWay <- TRUE # force stop
+      conti <- rle(sort(pickAge(i)))  # pick age of my contacts
+      npick <- conti$lengths  # of contact by ages
+      apick <- conti$values  # age-grp to pick
+      if (!all(apick %in% afree)) next  # if no free age, try again
+      ok <- sapply(seq_along(npick), function(x) npick[x] <= nfree[which(afree==apick[x])]) # if there are, check number enough?
+      if (all(ok)) isEnough <- TRUE
+    }
+    if (noWay) next  # leave this guy alone
+    pickedId <- unlist(sapply(seq_along(npick), function(x) pickId(x)))
+    if (any(which(ncont==0) %in% pickedId) ) break
+    g <- add_edges(g, makeLinks(i, pickedId) ) # add weights here if needed
+    ncont[i] <- 0  # update me, no more friend
+    ncont[pickedId] <- ncont[pickedId] - 1  # update friends
+    freeI <- which(ncont > 0) # update free id
+  }
+  g <- set_vertex_attr(g, 'age', value=age)
+  return(g)
+}
+genNet <- compiler::cmpfun(genNet)
+plotNet <- function(M, vs=log(degree(M)+2)*2, vc=vertex_attr(g, "age"), ly=layout_with_fr, ...) plot(g, vertex.size=vs, vertex.label=NA, edge.arrow.mode=0, layout=ly, vertex.color=vc, edge.color="gray90",...) 
